89925ddf055f504a650dd37e0aaf939a602f77e5
GarglemyGabagool <nickbhenderson95@gmail.com>
2023-09-18 18:04:15 -0500

    Bad commit practice, I know. But there's too many changes to list, but basically redid the entire game logic and page.

--- script.js
+++ script.js
@@ -1,47 +1,105 @@
-function game() {
-    let playerSelection;
-    let computerSelection;
-    let playerScore = 0;
-    let computerScore = 0;
+const playerChoices = document.querySelectorAll('[data-choice]');
+const finalColumn = document.querySelector('[data-final-column]');
+const playerScoreDisplay = document.querySelector('.player');
+const computerScoreDisplay = document.querySelector('.computer');
+let moves = 0;
+let playerScore = 0;
+let computerScore = 0;
+const CHOICES = [
+    {
+        name: 'rock',
+        emoji: '🪨',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '📜',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✂️',
+        beats: 'paper'
+    }
+]
 
-    function getComputerSelection() {
-        const computerChoice = ['rock', 'paper', 'scissors'];
-        const choiceNumber = Math.floor(Math.random() * 3);
-        return computerChoice[choiceNumber];
-    }
+playerChoices.forEach(playerChoice => {
+    playerChoice.addEventListener('click', e => {
+        const choiceName = playerChoice.dataset.choice;
+        const choice = CHOICES.find(choice => choice.name === choiceName);
+        determineWinner(choice);
 
-    function playRound() {
-        let playerSelection = prompt('Which do you choose?').toLowerCase();
-        let computerSelection = getComputerSelection().toLowerCase();
-        if (playerSelection == computerSelection) {
-            alert('It is a tie!');
+        const movesLeft = document.querySelector('.movesleft');
+        moves++;
+        movesLeft.innerText = `Moves left: ${20-moves}`;
+
+        if(moves == 20) {
+            gameOver(playerChoices, movesLeft);
         }
-        else if (
-            (computerSelection == 'rock' && playerSelection == 'scissors') ||
-            (computerSelection == 'paper' && playerSelection == 'rock') ||
-            (computerSelection == 'scissors' && playerSelection == 'paper')
-        ) {
-            alert('Automatons have won this round!');
-            computerScore++;
-        } else {
-            alert('You win the round!');
-            playerScore++;
-        }
-        console.log(playerScore, computerScore);
-        console.log(playerSelection, computerSelection);
-        if (playerScore < 5 && computerScore < 5) {
-            playRound();
-        } else {
-            declareWinner();
-        }
-    }
-    playRound();
-    function declareWinner() {
-        if (playerScore > computerScore) {
-            alert('Mankind wins the day!');
-        } else if (playerScore < computerScore) {
-            alert('The robot uprising is finally upon us.');
-        }
-    }
+    })
+})
+
+const determineWinner = (choice) => {
+    const computerChoice = getComputerChoice();
+    const playerWinner = getWinner(choice, computerChoice);
+    const computerWinner = getWinner(computerChoice, choice);
+    addChoiceResult(computerChoice, computerWinner);
+    addChoiceResult(choice, playerWinner);
+    
+    if(playerWinner) playerScore++, incrementScore(playerScoreDisplay);
+    if(computerWinner) computerScore++, incrementScore(computerScoreDisplay);
 }
-game()
+
+const incrementScore = (scoreSpan) => {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
+} 
+
+const addChoiceResult = (choice, winner) => {
+    const div = document.createElement('div');
+    div.innerText = choice.emoji;
+    div.classList.add('result-selection');
+    if (winner) div.classList.add('winner');
+    finalColumn.after(div);
+}
+
+const getWinner = (choice, opponentChoice) => {
+    return choice.beats === opponentChoice.name;
+}
+
+const getComputerChoice = () => {
+    const randomIndex = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[randomIndex];
+}
+
+const gameOver = (playerOptions, movesLeft) => {
+    const restart = document.querySelector('.choices');
+    const reloadBtn = document.createElement('button');
+    const results = document.createElement('h1');
+    results.classList.add('endgame');
+    results.style.color = 'white';
+    restart.appendChild(results)
+    playerChoices.forEach(choice => {
+        console.log(playerScore, computerScore)
+        choice.style.display = 'none';
+    })
+    movesLeft.style.display = 'none';
+
+    if (playerScore > computerScore) {
+        results.style.fontSize = '3rem';
+        results.innerText = 'You have won the game! Congratulations!'
+    } else if(playerScore < computerScore) {   
+        results.style.fontSize = '3rem';
+        results.innerText = 'You have lost the game! Try again next time!'
+    } else {
+        results.style.fontSize = '3rem';
+        results.innerText = 'It\'s a tie game! No one wins but you both lose!'
+    }
+    reloadBtn.classList.add('reload');
+    reloadBtn.innerText = 'Restart?'
+    reloadBtn.style.display = 'flex'
+    restart.appendChild(reloadBtn);
+    reloadBtn.addEventListener('click', e => {
+        window.location.reload();
+    })
+
+}
